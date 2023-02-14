0a6c5aa45491e78fc227d8c9243a0551e4791d7d
Justyn Spooner <justyn@dovu.io>
2023-02-14 00:03:34 +0000

    Testing allowance

--- src/allowance.ts
+++ src/allowance.ts
@@ -1,7 +1,11 @@
 import {
   AccountAllowanceApproveTransaction,
+  AccountBalanceQuery,
   AccountId,
+  Client,
+  ContractId,
   NftId,
+  TransferTransaction,
 } from "@hashgraph/sdk";
 
 import {
@@ -20,7 +24,6 @@ const CREATE_TOKEN_REQUIRED_ENVS = [
   "OPERATOR_PRIVATE_KEY",
   "TREASURY_ACCOUNT_ID",
   "TREASURY_PRIVATE_KEY",
-  "AUTO_RENEW_ACCOUNT_ID",
 ];
 
 dotenv.config();
@@ -33,9 +36,9 @@ async function main() {
   }
 
   // Read the token ID from the command line
-  let tokenId = TokenId.fromString(process.argv[2]);
+  let tokenToAllow = TokenId.fromString(process.argv[2]);
 
-  if (tokenId == null) {
+  if (tokenToAllow == null) {
     throw new Error("Pass the token ID as the first argument");
   }
 
@@ -51,30 +54,104 @@ async function main() {
     throw new Error("Pass the spender account ID as the third argument");
   }
 
-  const wallet = new Wallet(
-    process.env.OPERATOR_ACCOUNT_ID!,
-    process.env.OPERATOR_PRIVATE_KEY!,
-    new LocalProvider()
+  const ownerAccountKey = PrivateKey.fromString(
+    <string>process.env.OWNER_PRIVATE_KEY
   );
 
-  let transaction = await new AccountAllowanceApproveTransaction()
-    .approveTokenNftAllowanceAllSerials("0.0.15698", "0.0.1792", "0.0.2669")
-    .freezeWithSigner(wallet);
+  const spenderAccountKey = PrivateKey.fromString(
+    <string>process.env.SPENDER_PRIVATE_KEY
+  );
 
-  transaction = await transaction.signWithSigner(wallet);
+  const customerAccountId = AccountId.fromString("0.0.3067909");
+  const customerAccountKey = PrivateKey.fromString(
+    <string>process.env.CUSTOMER_PRIVATE_KEY
+  );
 
-  const response = await transaction.executeWithSigner(wallet);
+  const isMainnet = <string>process.env.HEDERA_NETWORK === "mainnet";
+
+  const client = isMainnet ? Client.forMainnet() : Client.forTestnet();
+  client.setOperator(ownerAccountId, ownerAccountKey);
+
+  let transaction = new AccountAllowanceApproveTransaction()
+    .approveTokenNftAllowanceAllSerials(
+      tokenToAllow,
+      ownerAccountId,
+      spenderAccountId
+    )
+    .freezeWith(client);
+
+  // let transaction = new AccountAllowanceApproveTransaction()
+  //   .approveTokenNftAllowance(
+  //     new NftId(tokenToAllow, 5),
+  //     ownerAccountId,
+  //     spenderAccountId
+  //   )
+  //   .freezeWith(client);
+
+  // let transaction = new AccountAllowanceApproveTransaction()
+  //   .approveHbarAllowance(
+  //     ownerAccountId,
+  //     spenderAccountId,
+  //     new Hbar(200, HbarUnit.Hbar)
+  //   )
+  //   .freezeWith(client);
+
+  const signTransaction = await transaction.sign(ownerAccountKey);
+
+  const response = await signTransaction.execute(client);
 
   console.log(response.toString());
 
-  const provider = wallet.getProvider();
+  const receipt = await response.getReceipt(client);
 
-  const transactionReceipt = await provider?.waitForReceipt(response);
-
-  console.log(transactionReceipt);
+  const transactionStatus = receipt.status;
 
   console.log(
-    `🎉 Spender ${spenderAccountId.toString()} can now sell Token ID ${tokenId.toString()} on behalf of owner ${ownerAccountId.toString()}`
+    "The allowance consensus status is " + transactionStatus.toString()
+  );
+
+  // // Test the allowance from the spender account
+  // client.setOperator(spenderAccountId, spenderAccountKey);
+
+  // // Create a transaction to transfer the NFT to another account from the spender
+  // const transferTransaction = new TransferTransaction()
+  //   .addApprovedNftTransfer(
+  //     new NftId(tokenToAllow, 10),
+  //     ownerAccountId,
+  //     customerAccountId
+  //   )
+  //   .addHbarTransfer(customerAccountId, new Hbar(-5, HbarUnit.Hbar))
+  //   .addHbarTransfer(ownerAccountId, new Hbar(5, HbarUnit.Hbar))
+  //   .freezeWith(client);
+
+  // //Sign the transaction with the spender account
+  // let signedTransferTransaction = await transferTransaction.sign(
+  //   spenderAccountKey
+  // );
+
+  // // Sign with the customer account
+  // signedTransferTransaction = await signedTransferTransaction.sign(
+  //   customerAccountKey
+  // );
+
+  // //Submit the transaction to a Hedera network
+  // const transferTransactionResponse = await signedTransferTransaction.execute(
+  //   client
+  // );
+
+  // //Request the receipt of the transaction
+  // const transferTransactionReceipt =
+  //   await transferTransactionResponse.getReceipt(client);
+
+  // //Get the transaction consensus status
+  // const transferTransactionStatus = transferTransactionReceipt.status;
+
+  // console.log(
+  //   "The transfer consensus status is " + transferTransactionStatus.toString()
+  // );
+
+  console.log(
+    `🎉 Spender ${spenderAccountId.toString()} can now sell Token ID ${tokenToAllow.toString()} on behalf of owner ${ownerAccountId.toString()}`
   );
 
   console.log(
